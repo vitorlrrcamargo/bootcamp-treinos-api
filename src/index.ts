3db41b5e619bf3f642ef738706eeeac95fafb7e9
Vitor Camargo <vitorcamargo@Vitors-Mac-mini.local>
2026-02-26 16:18:38 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
